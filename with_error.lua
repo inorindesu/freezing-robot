2deb7ed792b42f29e1c18b6ae1607d73be3438cc
Inori Sakura <inorindesu@gmail.com>
2013-03-24 20:32:44 +0800

    Added comments for lua scripts which demonstrats error handling of our program.

--- with_error.lua
+++ with_error.lua
@@ -1,3 +1,8 @@
+-- This script will trigger runtime error when running.
+-- (at 'a + nil')
+--
+-- Try loading it with the program and see what will happen!
+
 paramCount = 1
 calc = function(a)
      return a + nil
--- with_error2.lua
+++ with_error2.lua
@@ -1,3 +1,8 @@
+-- This script have an inconsistency between parameter count indicated
+-- in paramCount and real parameter count of calc function.
+--
+-- Try loading it with the program and see what will happen!
+
 paramCount = 1
 calc = function(a, b)
      return a + b
